2a5c428643bfee29d88f1faddadef636afd064a0
ncihnegn <ncihnegn@users.noreply.github.com>
2025-01-29 01:36:16 -0800

    Fix __init__

--- configs/paths.py
+++ configs/paths.py
@@ -1,24 +1,21 @@
 from dataclasses import dataclass, asdict, fields
 
 
-models_dir = "pretrained_models/"
-
-
 @dataclass(frozen=True)
 class DefaultPathsClass:
-    psp_path: str = models_dir + "psp_ffhq_encode.pt"
-    e4e_path: str = models_dir + "e4e_ffhq_encode.pt"
-    farl_path:str = models_dir + "face_parsing.farl.lapa.main_ema_136500_jit191.pt"
-    mobile_net_pth: str = models_dir + "mobilenet0.25_Final.pth"
-    ir_se50_path: str = models_dir + "model_ir_se50.pth"
-    stylegan_weights: str = models_dir + "stylegan2-ffhq-config-f.pt"
-    stylegan_car_weights: str = models_dir + "stylegan2-car-config-f-new.pkl"
-    stylegan_weights_pkl: str = models_dir + "stylegan2-ffhq-config-f.pkl"
-    arcface_model_path: str = models_dir + "iresnet50-7f187506.pth"
-    moco: str = models_dir + "moco_v2_800ep_pretrain.pt"
-    curricular_face_path: str = models_dir + "CurricularFace_Backbone.pth"
-    mtcnn: str = models_dir + "mtcnn"
-    landmark: str = models_dir + "79999_iter.pth"
+    psp_path: str = "pretrained_models/psp_ffhq_encode.pt"
+    e4e_path: str = "pretrained_models/e4e_ffhq_encode.pt"
+    farl_path:str = "pretrained_models/face_parsing.farl.lapa.main_ema_136500_jit191.pt"
+    mobile_net_pth: str = "pretrained_models/mobilenet0.25_Final.pth"
+    ir_se50_path: str = "pretrained_models/model_ir_se50.pth"
+    stylegan_weights: str = "pretrained_models/stylegan2-ffhq-config-f.pt"
+    stylegan_car_weights: str = "pretrained_models/stylegan2-car-config-f-new.pkl"
+    stylegan_weights_pkl: str = "pretrained_models/stylegan2-ffhq-config-f.pkl"
+    arcface_model_path: str = "pretrained_models/iresnet50-7f187506.pth"
+    moco: str = "pretrained_models/moco_v2_800ep_pretrain.pt"
+    curricular_face_path: str = "pretrained_models/CurricularFace_Backbone.pth"
+    mtcnn: str = "pretrained_models/mtcnn"
+    landmark: str = "pretrained_models/79999_iter.pth"
 
     def __iter__(self):
       for field in fields(self):
