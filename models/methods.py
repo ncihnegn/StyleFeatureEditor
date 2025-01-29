4a768f4983b4e556c30deb9cd6b81f9764d0bc9c
ncihnegn <ncihnegn@users.noreply.github.com>
2025-01-29 01:39:09 -0800

    Remove super __init__

--- models/methods.py
+++ models/methods.py
@@ -29,7 +29,7 @@ class FSEFull(nn.Module):
                  paths=DefaultPathsClass(),
                  checkpoint_path=None,
                  inverter_pth=None):
-        super(FSEFull, self).__init__()
+        #super(FSEFull, self).__init__()
         self.opts = {
             "device": device,
             "checkpoint_path": checkpoint_path,
